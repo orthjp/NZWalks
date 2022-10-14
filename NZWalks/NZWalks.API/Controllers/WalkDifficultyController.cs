f0027db8ea5295b6979e5122c38fa60a462eda9a
orthjp <orthjp@gmail.com>
2022-10-14 15:28:08 -0400

    Section 6 updates

---
+++ NZWalks/NZWalks.API/Controllers/WalkDifficultyController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultyController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultyController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficulties()
+        {
+            var difficulties = await walkDifficultyRepository.GetAllAsync();
+            return Ok(difficulties);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkDifficulty")]
+        public async Task<IActionResult> GetWalkDifficulty(Guid id)
+        {
+            var difficulty = await walkDifficultyRepository.GetAsync(id);
+
+            if (difficulty == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(difficulty);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficulty(Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            //Convert DTO to Domain model
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = addWalkDifficultyRequest.Code
+            };
+
+            // Call repository
+            walkDifficultyDomain = await walkDifficultyRepository.AddAsync(walkDifficultyDomain);
+
+            //Convert back to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficultyDomain);
+            return CreatedAtAction(nameof(GetWalkDifficulty), new { id = walkDifficultyDTO.Id }, walkDifficultyDTO);
+
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkDifficulty(Guid id)
+        {
+            // get walk difficulty from database
+            var walkDifficulty = await walkDifficultyRepository.DeleteAsync(id);
+
+            // if we dont get one return not found
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            // convert to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+
+
+            // return ok response
+            return Ok(walkDifficultyDTO);
+
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkDifficulty([FromRoute] Guid id, [FromBody] Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            var walkDifficulty = new Models.Domain.WalkDifficulty()
+            {
+                Code = updateWalkDifficultyRequest.Code
+            };
+
+            // update region using repository
+            walkDifficulty = await walkDifficultyRepository.UpdateAsync(id, walkDifficulty);
+
+            // if null then return not found
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            // convert domain back to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+
+            // return ok response
+            return Ok(walkDifficultyDTO);
+        }
+
+    }
+}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalksRepository, WalksRepository>();
+builder.Services.AddScoped<IWalkDifficultyRepository, WalkDifficultyRepository>();
 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
